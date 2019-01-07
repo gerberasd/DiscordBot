b0accb19714f7687ef4df54ceb4655681797f0b7
gerberasd <46430027+gerberasd@users.noreply.github.com>
2019-01-07 17:16:08 +0900

    Update slot.py

--- slot.py
+++ slot.py
@@ -12,11 +12,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-   if message.content.startswith('!334'):
-        slot_list = ['3', '3', '4'] # スロットにしたいものをここへ
+    if message.content.startswith('!334'): #!334で実行
+        slot_list = ['3','3','4'] #スロット内容
         A = random.choice(slot_list)
         B = random.choice(slot_list)
         C = random.choice(slot_list)
-        await client.send_message(message.channel, "%s%s%s" % (A, B, C))
+        await client.send_message(message.channel, "%s%s%s" % (A,B,C))
 
 client.run('とーくんをここに貼り付け')
